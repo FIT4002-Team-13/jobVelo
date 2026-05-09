2c74fbf3d955147df9785c13ae6302c71cb42671
geoff023 <ghii0002@student.monash.edu>
2026-05-09 14:06:39 +1000

    feat: added a dashboard page to test the functionality of the login and signup

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-extrabold tracking-tight">Jobvelo dashboard</h1>
+    </div>
+  )
+}
