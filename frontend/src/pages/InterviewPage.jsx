ec01add4667dff742d63a1fa8903b8953802c253
cameron <chum0001@student.monash.edu>
2026-05-25 23:30:16 +1000

    Changed to comment icon

--- frontend/src/pages/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage.jsx
@@ -150,8 +150,7 @@ function TranscriptEntry({ entry }) {
           stroke="currentColor"
           strokeWidth="2"
         >
-          <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
-          <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+          <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
         </svg>
       </button>
     </div>
