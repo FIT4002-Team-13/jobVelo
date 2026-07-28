22dfdedae258e867ae867081bf5aa007a4d86de2
cameron <chum0001@student.monash.edu>
2026-07-28 12:51:21 +1000

    feat: Expanded upon current backend tests

---
+++ backend/tests/conftest.py
@@ -0,0 +1,18 @@
+from unittest.mock import AsyncMock, patch
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+@pytest.fixture
+def client():
+    with (
+        #Patch intercepts database functions and replaces them with async mock
+        patch("database.connect_to_mongo", new_callable=AsyncMock),
+        patch("database.ensure_indexes", new_callable=AsyncMock),
+        patch("database.close_mongo_connection", new_callable=AsyncMock),
+    ):
+        with TestClient(app) as c:
+            #Hands test client to each test that asks for it and cleans up after
+            yield c
+
