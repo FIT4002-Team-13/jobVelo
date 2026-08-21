19dff6bdab891ecc5f3d1c681202252a7ca66d2b
Tinesia <161189071+Tinesia@users.noreply.github.com>
2026-08-21 21:11:20 +1000

    Feature/us20 suggest technical and behavioural questions (#13)
    * feat: wrote the the model and the openai service.
    - added a model for question responses
    - add openai service to generate question based on job description
    - add api rout for the job data and generate question
    - Need to test it after api key is obtained.
    
    * feat: implemented suggest more like this function
    
    * Implemented suggest more like this function
    
    * fix: accidentaly changed package-lock.json
    
    * feat: completed frontend for question suggestion and fixed some backend implementation problem:
    - Made candidate detail page's start interview button work
    - Added suggested question to interview page
    - made suggest more and ignore work
    - made minimum of 6 questions to be shown
    
    * fix: Fixed errors found by the CI
    
    * fix: Changed to always having 2 questions except when you p-ressed more like this.
    Mage generating questions and error message in the center of the box.
    
    * fix: adjusted question texbox height
    
    * fix: added hide scrollbar to index and hide the scroll bar for question cards
    
    * fix: fixed the issue where 2 question of same category will be generated when the original 2 questions are deleted simutaneously
    
    * fix: removed unused function
    
    ---------
    
    Co-authored-by: TinesiaY <chuenting2013yu@gmail.com>

---
+++ backend/models/interview_question.py
@@ -0,0 +1,23 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class SimilarQuestion(BaseModel):
+    original_question: str
+    category: Literal["technical", "behavioural"]
+
+
+class SimilarQuestionResult(BaseModel):
+    category: Literal["technical", "behavioural"]
+    question: str
+    reason: str
+
+class SuggestedQuestion(BaseModel):
+    category: Literal["technical", "behavioural"]
+    question: str
+    source: str
+    reason: str
+
+class SuggestedQuestionsList(BaseModel):
+    questions: list[SuggestedQuestion]
